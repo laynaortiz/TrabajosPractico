1f02912c99e7d7b2cae38c819da0a546088ae6bd
laynaortiz <52211334@ivt.edu.ar>
2026-06-08 11:07:14 -0300

    8.6.26

---
+++ TP5/app.js
@@ -0,0 +1,60 @@
+let suma
+let resta
+let multiplicacion
+let division
+
+
+suma = 6 + 7
+resta = 20 - 10
+multiplicacion = 7 * 6
+division = 9 / 3
+
+
+alert(suma)
+alert(resta)
+
+
+console.log(multiplicacion)
+console.log(division)
+
+
+alert("El resultado de la suma de 6 + 7 es " +  suma)
+alert("El resultado de la resta de 20 - 10 es " +  resta)
+
+
+console.log("El resultado de la multiplicacion de 7 * 6 es " +  multiplicacion)
+console.log("El resultado de la division de 9 / 3 es " +  division)
+
+
+let nombreAlumno = "Valen"
+let apellidoAlumno = "Veggeta"
+let edadAlumno = 15
+
+
+console.log("El alumno " + nombreAlumno  +" "+ apellidoAlumno + " tiene " + edadAlumno + " años.")
+
+
+let precio1 = 29
+let precio2 = 5
+let precio3 = 26
+
+
+let total = precio1 + precio2 + precio3
+
+
+console.log("El total a pagar es "+ total)
+
+
+let comidaFavorita = "ñoquis"
+let colorFavorito = "negro"
+let numeroFavorito = "5"
+
+
+console.log("Mi comida favorita son los "+comidaFavorita+", mi color favorito es el "+colorFavorito+ " y mi número favorito es el "+numeroFavorito)
+
+
+let pais
+pais = "Argentina"
+console.log(pais)
+pais = "Brasil"
+console.log(pais)
